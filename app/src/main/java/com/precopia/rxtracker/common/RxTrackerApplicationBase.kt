a02496e0aa2706c8a623ac2ff0e55a9330266984
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-03-05 17:01:54 -0500

    Initialized RxAndroid's scheduler to not wait for vsync

--- app/src/main/java/com/precopia/rxtracker/common/RxTrackerApplicationBase.kt
+++ app/src/main/java/com/precopia/rxtracker/common/RxTrackerApplicationBase.kt
@@ -1,5 +1,25 @@
 package com.precopia.rxtracker.common
 
 import android.app.Application
+import android.os.Looper
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
-internal abstract class RxTrackerApplicationBase : Application()
+internal abstract class RxTrackerApplicationBase : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initRxAndroidScheduler()
+    }
+
+    /**
+     * This means events flowing to the main thread do not
+     * have to wait for vsync, decreasing the likelihood of frame drops.
+     * https://twitter.com/jakewharton/status/1170437658776133636?s=12
+     */
+    private fun initRxAndroidScheduler() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler {
+            AndroidSchedulers.from(Looper.getMainLooper(), true)
+        }
+    }
+}
