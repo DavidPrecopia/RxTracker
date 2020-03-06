e5fb4f5b45c1995471cccc24070e4ef3f6a18bc6
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-03-05 22:00:35 -0500

    Implemented TimeStampDiffCallback

---
+++ app/src/main/java/com/precopia/rxtracker/view/timelistview/TimeStampDiffCallback.kt
@@ -0,0 +1,12 @@
+package com.precopia.rxtracker.view.timelistview
+
+import androidx.recyclerview.widget.DiffUtil
+import com.precopia.domain.datamodel.TimeStamp
+
+class TimeStampDiffCallback : DiffUtil.ItemCallback<TimeStamp>() {
+    override fun areItemsTheSame(oldItem: TimeStamp, newItem: TimeStamp) =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: TimeStamp, newItem: TimeStamp) =
+        oldItem == newItem
+}
---
+++ app/src/test/java/com/precopia/rxtracker/view/timelistview/TimeStampDiffCallbackTest.kt
@@ -0,0 +1,37 @@
+package com.precopia.rxtracker.view.timelistview
+
+import com.precopia.domain.datamodel.TimeStamp
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class TimeStampDiffCallbackTest {
+
+
+    private val one = TimeStamp(1, "titleOne", "timeOne")
+    private val two = TimeStamp(2, "titleTwo", "timeTwo")
+
+
+    private val diffCallback = TimeStampDiffCallback()
+
+
+    @Test
+    fun `areItemsTheSame - true`() {
+        assertThat(diffCallback.areItemsTheSame(one, one)).isTrue()
+    }
+
+    @Test
+    fun `areItemsTheSame - false`() {
+        assertThat(diffCallback.areItemsTheSame(one, two)).isFalse()
+    }
+
+
+    @Test
+    fun `areContentsTheSame - true`() {
+        assertThat(diffCallback.areContentsTheSame(one, one)).isTrue()
+    }
+
+    @Test
+    fun `areContentsTheSame - false`() {
+        assertThat(diffCallback.areContentsTheSame(one, two)).isFalse()
+    }
+}
