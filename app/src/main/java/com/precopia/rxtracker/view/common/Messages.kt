7cfe4e962f819fc621fa3959007fff22d60940ef
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-03-17 16:59:09 -0400

    Renamed ErrorMessages file to Message and added a success message

--- app/src/main/java/com/precopia/rxtracker/view/common/Messages.kt
+++ app/src/main/java/com/precopia/rxtracker/view/common/Messages.kt
@@ -1,5 +1,7 @@
 package com.precopia.rxtracker.view.common
 
+const val MSG_SUCCESSFULLY_SAVE = "Successfully saved"
+
 const val ERROR_GENERIC = "An error as occurred"
 
 const val ERROR_EMPTY_LIST = "This is empty"
