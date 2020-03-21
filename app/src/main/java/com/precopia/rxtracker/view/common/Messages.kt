24657c153e62ff083ef8d57fe182e908c0887ce2
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-03-21 14:31:21 -0400

    Added error message to Messages

--- app/src/main/java/com/precopia/rxtracker/view/common/Messages.kt
+++ app/src/main/java/com/precopia/rxtracker/view/common/Messages.kt
@@ -6,4 +6,6 @@ const val ERROR_GENERIC = "An error as occurred"
 
 const val ERROR_EMPTY_LIST = "This is empty"
 
+const val ERROR_TITLE = "Title cannot be empty"
+
 const val ERROR_OPERATION_FAILED = "Operation failed"
