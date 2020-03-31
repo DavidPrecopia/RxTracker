46a625407dc76bc109b2d962d9483b35ace601a2
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-03-31 12:50:00 -0400

    Expanded TimeStamp and Prescription Repos' unit tests

---
+++ data/src/test/java/com/precopia/data/repository/PrescriptionRepoTest.kt
@@ -0,0 +1,97 @@
+package com.precopia.data.repository
+
+import com.precopia.data.dao.PrescriptionDao
+import com.precopia.data.datamodel.DbPrescription
+import com.precopia.domain.datamodel.Prescription
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class PrescriptionRepoTest {
+
+
+    private val dao = mockk<PrescriptionDao>()
+
+
+    private val repo = PrescriptionRepo(dao)
+
+
+    @BeforeEach
+    fun setUp() {
+        clearAllMocks()
+    }
+
+
+    @Nested
+    inner class GetAll {
+        /**
+         * - Verify that an RxJava 3 Flowable is returned of type [Prescription].
+         * - It will be successful in this test.
+         */
+        @Test
+        fun `getAll - success`() {
+            val dbPrescription = DbPrescription(0, "title")
+
+            every { dao.getAll() } returns io.reactivex.Flowable.just(listOf(dbPrescription))
+
+            repo.getAll()
+                    .test()
+                    .assertValue {
+                        it[0].id == dbPrescription.id && it[0].title == dbPrescription.title
+                    }
+        }
+
+        /**
+         * - Verify that an RxJava 3 Flowable is returned of type [Prescription].
+         * - It will return an Throwable in this test.
+         */
+        @Test
+        fun `getAll - failure`() {
+            val throwable = mockk<Throwable>(relaxed = true)
+
+            every { dao.getAll() } returns io.reactivex.Flowable.error(throwable)
+
+            repo.getAll()
+                    .test()
+                    .assertError(throwable)
+        }
+    }
+
+
+    @Nested
+    inner class Add {
+        /**
+         * - Verify that an RxJava 3 Completable is returned.
+         * - It will complete in this test.
+         */
+        @Test
+        fun `add - complete`() {
+            val dbPrescription = DbPrescription(0, "title")
+
+            every { dao.add(dbPrescription) } returns io.reactivex.Completable.complete()
+
+            repo.add(dbPrescription.title)
+                    .test()
+                    .assertComplete()
+        }
+
+        /**
+         * - Verify that an RxJava 3 Completable is returned.
+         * - It will error in this test.
+         */
+        @Test
+        fun `add - error`() {
+            val dbPrescription = DbPrescription(0, "title")
+            val throwable = mockk<Throwable>(relaxed = true)
+
+            every { dao.add(dbPrescription) } returns io.reactivex.Completable.error(throwable)
+
+            repo.add(dbPrescription.title)
+                    .test()
+                    .assertError(throwable)
+        }
+    }
+}
--- data/src/test/java/com/precopia/data/repository/TimeStampRepoTest.kt
+++ data/src/test/java/com/precopia/data/repository/TimeStampRepoTest.kt
@@ -3,13 +3,14 @@ package com.precopia.data.repository
 import com.precopia.data.dao.TimeStampDao
 import com.precopia.data.datamodel.DbTimeStamp
 import com.precopia.data.util.ICurrentTimeUtil
+import com.precopia.domain.datamodel.TimeStamp
 import io.mockk.CapturingSlot
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import io.reactivex.Completable
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class TimeStampRepoTest {
@@ -29,23 +30,101 @@ internal class TimeStampRepoTest {
     }
 
 
-    /**
-     * - Accept the prescription's title as a parameter.
-     * - Acquire the current date and time.
-     * - Create a new instance of [DbTimeStamp].
-     * - Pass the instance to the Dao.
-     */
-    @Test
-    fun delete() {
-        val capturedArg = CapturingSlot<DbTimeStamp>()
-        val rxTitle = "title"
-        val currentTimeString = "time"
+    @Nested
+    inner class GetAll {
+        /**
+         * - Verify that an RxJava 3 Flowable is returned of type [TimeStamp].
+         * - It will be successful in this test.
+         */
+        @Test
+        fun `getAll - success`() {
+            val dbTimeStamp = DbTimeStamp(0, "title", "time")
 
-        every { timeUtil.currentTime() } returns currentTimeString
-        every { dao.add(timeStamp = capture(capturedArg)) } answers { Completable.complete() }
+            every { dao.getAll() } returns io.reactivex.Flowable.just(listOf(dbTimeStamp))
 
-        repo.add(rxTitle)
+            repo.getAll()
+                    .test()
+                    .assertValue {
+                        it[0].id == dbTimeStamp.id && it[0].title == dbTimeStamp.title
+                    }
+        }
 
-        verify { dao.add(capturedArg.captured) }
+        /**
+         * - Verify that an RxJava 3 Flowable is returned of type [TimeStamp].
+         * - It will return an Throwable in this test.
+         */
+        @Test
+        fun `getAll - failure`() {
+            val throwable = mockk<Throwable>(relaxed = true)
+
+            every { dao.getAll() } returns io.reactivex.Flowable.error(throwable)
+
+            repo.getAll()
+                    .test()
+                    .assertError(throwable)
+        }
+    }
+
+
+    @Nested
+    inner class Add {
+        /**
+         * - Verify that an RxJava 3 Completable is returned.
+         * - It will complete in this test.
+         */
+        @Test
+        fun `add - complete`() {
+            val dbTimeStamp = DbTimeStamp(0, "title", "time")
+
+            every { dao.add(dbTimeStamp) } returns io.reactivex.Completable.complete()
+            every { timeUtil.currentTime() } returns dbTimeStamp.time
+
+            repo.add(dbTimeStamp.title)
+                    .test()
+                    .assertComplete()
+        }
+
+        /**
+         * - Verify that an RxJava 3 Completable is returned.
+         * - It will error in this test.
+         */
+        @Test
+        fun `add - error`() {
+            val dbTimeStamp = DbTimeStamp(0, "title", "time")
+            val throwable = mockk<Throwable>(relaxed = true)
+
+            every { dao.add(dbTimeStamp) } returns io.reactivex.Completable.error(throwable)
+            every { timeUtil.currentTime() } returns dbTimeStamp.time
+
+            repo.add(dbTimeStamp.title)
+                    .test()
+                    .assertError(throwable)
+        }
+    }
+
+
+    @Nested
+    inner class Delete {
+        /**
+         * - Accept the prescription's title as a parameter.
+         * - Acquire the current date and time.
+         * - Create a new instance of [DbTimeStamp].
+         * - Pass the instance to the Dao.
+         */
+        @Test
+        fun delete() {
+            val capturedArg = CapturingSlot<DbTimeStamp>()
+            val rxTitle = "title"
+            val currentTimeString = "time"
+
+            every { timeUtil.currentTime() } returns currentTimeString
+            every { dao.add(timeStamp = capture(capturedArg)) } answers {
+                io.reactivex.Completable.complete()
+            }
+
+            repo.add(rxTitle)
+
+            verify { dao.add(capturedArg.captured) }
+        }
     }
 }
