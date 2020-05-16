7bcb34670f55bae478ee26641977a00ebaa5d06c
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-05-15 23:49:05 -0400

    Unit tests for PrescriptionDao covering the modification of position
    They were egregiously missing.

--- data/src/androidTest/java/com/precopia/data/database/PrescriptionDaoTest.kt
+++ data/src/androidTest/java/com/precopia/data/database/PrescriptionDaoTest.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.precopia.data.datamodel.DbPrescription
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Rule
 import org.junit.Test
@@ -21,9 +22,9 @@ internal class PrescriptionDaoTest {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private val database = Room.inMemoryDatabaseBuilder(
-                    ApplicationProvider.getApplicationContext<Application>(),
-                    RxTrackerDatabase::class.java
-            )
+            ApplicationProvider.getApplicationContext<Application>(),
+            RxTrackerDatabase::class.java
+    )
             .allowMainThreadQueries()
             .build()
 
@@ -58,6 +59,127 @@ internal class PrescriptionDaoTest {
                 .test()
                 .assertValue { it.size == 1 && it[0].title == titleString }
     }
+
+    /**
+     * - Clear the database.
+     * - Add a [DbPrescription].
+     * - Retrieve it to gets its ID.
+     * - Update is position and verify the change was made.
+     */
+    @Test
+    fun updatePositionAndGetAll() {
+        val positionOriginal = 0.0
+        val positionModified = 1.0
+        val prescription = DbPrescription(title = "title", position = positionOriginal)
+
+        dao.deleteAll()
+
+        dao.add(prescription)
+                .test()
+                .assertComplete()
+
+        val insertedId = dao.getAll()
+                .test()
+                .values()[0][0].id
+
+        assertThat(
+                dao.updatePositionAndGetAll(insertedId, positionModified)[0].position
+        ).isEqualTo(
+                positionModified
+        )
+    }
+
+    /**
+     * - Clear the database.
+     * - Add a [DbPrescription].
+     * - Retrieve it to get its ID.
+     * - Modify its position with its ID.
+     * - Retrieve the inserted [DbPrescription] and verify the change was made.
+     */
+    @Test
+    fun updatePosition() {
+        val positionOriginal = 0.0
+        val positionModified = 1.0
+        val prescription = DbPrescription(title = "title", position = positionOriginal)
+
+        dao.deleteAll()
+
+        dao.add(prescription)
+                .test()
+                .assertComplete()
+
+        val insertedId = dao.getAll()
+                .test()
+                .values()[0][0].id
+
+        dao.updatePosition(insertedId, positionModified)
+
+        dao.getAll()
+                .test()
+                .assertValue { it[0].position == positionModified }
+    }
+
+    /**
+     * - Clear the database.
+     * - Add a [DbPrescription].
+     * - Retrieve it.
+     * - Verify that it is unmodified.
+     */
+    @Test
+    fun getAllSynchronously() {
+        val title = "title"
+        val position = 0.0
+        val prescription = DbPrescription(title = title, position = position)
+
+        dao.deleteAll()
+
+        dao.add(prescription)
+                .test()
+                .assertComplete()
+
+        dao.getAll()
+                .test()
+                .assertValue { it[0].title == title && it[0].position == position }
+    }
+
+    /**
+     * - Clear the database.
+     * - Add two [DbPrescription].
+     * - Retrieve both.
+     * - Modify both of their respective positions.
+     * - Retrieve both are verify that the changes have been made.
+     */
+    @Test
+    fun updateAllPositions() {
+        val prescriptionOne = DbPrescription(title = "titleOne", position = 0.0)
+        val positionOneModified = prescriptionOne.position + 1
+        val prescriptionTwo = DbPrescription(title = "titleTwo", position = 1.0)
+        val positionTwoModified = prescriptionTwo.position + 1
+
+        dao.deleteAll()
+
+        dao.add(prescriptionOne)
+                .test()
+                .assertComplete()
+
+        dao.add(prescriptionTwo)
+                .test()
+                .assertComplete()
+
+        val insertedPrescriptionsList = dao.getAllSynchronously()
+
+        insertedPrescriptionsList[0].position = positionOneModified
+        insertedPrescriptionsList[1].position = positionTwoModified
+
+        dao.updateAllPositions(insertedPrescriptionsList)
+                .test()
+                .assertComplete()
+
+        val postModificationList = dao.getAllSynchronously()
+        assertThat(postModificationList[0].position).isEqualTo(positionOneModified)
+        assertThat(postModificationList[1].position).isEqualTo(positionTwoModified)
+    }
+
 
     /**
      * - Clear the database.
